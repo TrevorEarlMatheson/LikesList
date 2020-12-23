8aa4169e3906e19929ac4142d11d9f3a591857af
Trevor Earl Matheson <trevor.earl.matheson@gmail.com>
2020-12-23 14:04:37 -0700

    Prog. takes names/likes and displays who liked it.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LikesList
 {
     class Program
     {
+        static List<string> GetNames(List<string> likes)
+        {
+            Console.WriteLine("Enter a name: ");
+            var name = Console.ReadLine();
+            if(name.Replace(" ","") == "") //If the entry does not contain anything other than white space return.
+            {
+                return(likes);
+            }
+            else
+            {
+                likes.Add(name);
+                return(GetNames(likes)); //Else prompt user for another name.
+            }
+
+        }
+
+        static string GetLikes()
+        {
+            var names = GetNames(new List<String>());
+            if(names.Count == 0)
+            {
+                return("Zero Likes");
+            }
+            else if(names.Count == 1)
+            {
+                return(names[0] + " liked your post.");
+            }
+            else if(names.Count == 2)
+            {
+                return(names[0] + " and " + names[1] + " liked your post.");
+            }
+            else if(names.Count == 3)
+            {
+                return(names[0] + ", " + names[1] + " and " + (names.Count-2) + " other liked your post.");
+            }
+            else
+            {
+                return(names[0] + ", " + names[1] + " and " + (names.Count-2) + " others liked your post.");
+            }
+        }
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(GetLikes());
         }
     }
 }
